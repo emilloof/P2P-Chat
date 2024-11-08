b7ae60b57ae18e0ece41f3beab735c4c51a70be7
emilo851 <emilo851@student.liu.se>
2024-11-08 15:22:52 +0100

    init

---
+++ ChatApp/ChatApp/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.ViewModel
+{
+    internal class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public ObservableCollection<String> Messages { get; private set; }
+
+        public MainWindowViewModel() {
+            Messages = new ObservableCollection<string>();
+            Messages.Add("Test message");
+
+
+        }
+    }
+}
